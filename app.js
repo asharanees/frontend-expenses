3ca4c994261a251c6c9be251f7e8efcd3925751e
ashar <asharanees@gmail.com>
2025-07-05 16:49:35 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+
+async function fetchExpenses() {
+    console.log("Button clicked");
+  const userId = document.getElementById('userId').value;
+  const start = document.getElementById('start').value;
+  const end = document.getElementById('end').value;
+  const category = document.getElementById('category').value;
+
+  const params = new URLSearchParams({ userId });
+  if (start) params.append('start', start + 'T00:00');
+  if (end) params.append('end', end + 'T23:59');
+  if (category) params.append('category', category);
+
+ const apiUrl = "https://ylgl5p7917.execute-api.us-east-1.amazonaws.com/prod";
+ const res = await fetch(`${apiUrl}/expenses?${params.toString()}`);
+  
+  const data = await res.json();
+
+  document.getElementById('summary').textContent =
+    `Total: ${data.totalAmount} | Count: ${data.count}`;
+
+  const ul = document.getElementById('results');
+  ul.innerHTML = "";
+  data.data.forEach(item => {
+  const li = document.createElement('li');
+
+  // Format timestamp
+  const formattedDate = new Date(item.timestamp).toLocaleString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit"
+  });
+
+  li.textContent = `${formattedDate} – ${item.category} – $${item.amount}`;
+  li.className = "border p-2 rounded bg-gray-50"; // optional styling
+  ul.appendChild(li);
+});
+
+}
